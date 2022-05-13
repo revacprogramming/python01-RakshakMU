e65cd11b9db83d92164fe30f0221f0dc7d971f79
RakshakMU <97020781+RakshakMU@users.noreply.github.com>
2022-05-13 13:40:59 +0000

    06

--- ActivitySet01/problem06.py
+++ ActivitySet01/problem06.py
@@ -1,16 +1,45 @@
-# Loops & Iterators
+'''def computepay(h,r):
+  if h < 40:
+    pay = (h*r)
+  else:
+    pay = 40*r+((h-40)*(1.5*r))
+  return pay 
 
-largest = None
-smallest = None
+hrs=input("Enter hrs : ")
+h=float(hrs)
+rate=input("Enter rate :")
+r=float(rate)
 
-while True:
-    num = input("Enter a number? ")
+pay = computepay(h,r)
+print(pay)'''
 
-    if num == "done":
-        break
 
-    # ...
 
-    print(num)
+'''def computepay(h, r):
+    if h<=40:
+        return h*r
+    elif h>40:
+        h1=h-40
+        fee=40*r+h1*r*1.5
+        return fee
+hrs = float(input("Enter hours? "))
+rte = float(input("Enter rate per hour? "))
+p = computepay(hrs, rte)
+print("Pay", p)'''
 
-print("Maximum", largest)
+
+
+def computepay(h,r):
+  
+  if h<40:
+    return h*r
+  else:
+    pay=40*r+((h-40)*1.5*r)
+    return pay
+h=float(input("Enter hrs"))
+r=float(input("Enter rate"))    
+p=computepay(h,r)
+print(p)
+
+#h=float(input("Enter hrs"))
+#r=float(input("Enter rate")) these two cannot be written inside def function
